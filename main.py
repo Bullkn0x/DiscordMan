f83096d05558e05d50508c20caa640c596a46246
bullkn0x <richduchin@gmail.com>
2019-09-16 14:33:22 -0400

    added input functionality to bot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 import discord
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,4 +17,28 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if '$FINDSTOCK' in message.content:
+        try:
+            ticker = message.content.split(' ')[1]
+            await message.channel.send(ticker)
+        except IndexError:
+            await  message.channel.send('Please Input Ticker Symbol. Ex: $FINDSTOCK AAPL')
+
+
+
+test = ['qi chen', 'rich duchin']
 client.run(TOKEN)
