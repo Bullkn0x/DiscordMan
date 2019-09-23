f71e773bae4592a6831baa2ddfec64863e64fc65
bullkn0x <richduchin@gmail.com>
2019-09-23 17:27:14 -0400

    imagebank and cleaned imagesave function

--- content_detection/imagesave.py
+++ content_detection/imagesave.py
@@ -6,4 +6,3 @@ def imageSaver(url):
     with open('imagebank/downloaded_image.jpg', 'wb') as handler:
         handler.write(img_data)
 
-imageSaver('https://2.bp.blogspot.com/-1F4CIsa0uJU/WQEao4Vn80I/AAAAAAAABTA/FBI7wnx0ryw2JRa3RC0O9Q2k35rNEr6ZgCLcB/s640/post_6_1.png')
