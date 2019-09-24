a5d67539368996ccb8d2f2f7455b49654a935ccd
bullkn0x <richduchin@gmail.com>
2019-09-24 17:18:05 -0400

    quick alteration to weather api to access live timestamp

--- weathercall.py
+++ weathercall.py
@@ -9,14 +9,13 @@ load_dotenv()
 DARK_TOKEN=os.getenv('DARK_SKY_API')
 
 def weatherdata():
-    timestamp = 1569351970
     url = f'https://api.darksky.net/forecast/{DARK_TOKEN}/42.3601,-71.0589'
     response = requests.get(url)
     print("response is ",response)
     data = response.text
     json_data = json.loads(data)
     print(json_data)
-    dt_object = datetime.fromtimestamp(timestamp)
+    dt_object = datetime.fromtimestamp(json_data['currently']['time'])
     print("TIME IS: ", dt_object)
     #print("type(dt_object) =", type(dt_object))
 
