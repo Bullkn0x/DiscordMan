0aef386d0e1fd5085f8ea2329ece61c5e6df7cbb
Qi <qchen125@gmail.com>
2019-09-25 14:27:30 -0400

    formatting weather API 7 days forecast

--- weathercall.py
+++ weathercall.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 import json
 import os
@@ -14,9 +14,22 @@ def weatherdata():
     print("response is ",response)
     data = response.text
     json_data = json.loads(data)
-    print(json_data)
+    #print(json_data)
     dt_object = datetime.fromtimestamp(json_data['currently']['time'])
     print("TIME IS: ", dt_object)
+
+
+    # adding and subtracting time using +-timedelta(days, minutes, seconds)
+    for each in json_data['daily']['data']:
+        print("Date: ", (datetime.fromtimestamp(each['time'])).strftime('%m/%d/%Y'))
+        print("Summary ", each['summary'])
+        print("High ", each['temperatureHigh'])
+        print("Low ", each['temperatureLow'])
+
+    #print("time is " , datetime.fromtimestamp(json_data['daily']['data'][0]['time']))
+    #print("time is " , datetime.fromtimestamp(json_data['daily']['data'][1]['time']))
+    #print("time is " , datetime.fromtimestamp(json_data['daily']['data'][2]['time']))
+    #print("time is " , datetime.fromtimestamp(json_data['daily']['data'][3]['time']))
     #print("type(dt_object) =", type(dt_object))
 
 if __name__ == '__main__':
